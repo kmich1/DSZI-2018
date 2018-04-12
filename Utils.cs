862a3b73bfa32478b0c18ed39cc2c379b328cbb7
Krzysztof Michalak <k.michalak.gajda@gmail.com>
2018-04-12 18:11:43 +0200

    Add global random generation

---
+++ Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSZI_2018
+{
+    public static class Utils
+    {
+        private static Random rnd = new Random();
+        public static int GetRandom(int begin, int end) => rnd.Next(begin, end);
+    }
+}
